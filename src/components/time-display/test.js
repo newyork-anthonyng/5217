281f896f651cf8a2742ae42e2afd0cefb2df5bd6
Anthony Ng <newyork.anthonyng@gmail.com>
2017-06-24 09:29:34 -0700

    Initial commit

---
+++ src/components/time-display/test.js
@@ -0,0 +1,12 @@
+var expect = require('chai').expect;
+
+test('should render time in milliseconds as minutes', function(context) {
+  // var timeInMs = 2 * 60 * 1000; // 2 minutes
+  // var output = context.render({ timeInMs: timeInMs });
+  // console.log(output.$('h1').html);
+  // console.log(output.$('h1').text);
+  // // console.log(output);
+  // // console.log(output.html);
+  // // console.log(output.text);
+  // // expect(output.html).to.equal('<h1>2</h1>');
+});
